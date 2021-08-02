47f0262500bb564911aa806c308d4ac0030fe1c4
joe-bevan <joebevan80@gmail.com>
2021-08-02 13:01:53 +0100

    Uploaded profiler

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# profiler
+# C++ profiler
 A single header scoped-based C++ timer class for profiling function execution times.
+
+## Examples
+```cpp
+void Foo() { ... }
+
+void Bar() 
+{
+    {
+        Profiler p{"Foo() timer"};
+        Foo();
+    }
+}
+```
+Output:
+[Foo() timer] elapsed time: 2029 ms
+
+The elapsed time can be retrieved at any point by calling
+`GetElapsedTime()` on a valid profiler. Calling this function does not reset the timer.
+
+## Optional
+The profiler time format can be changed to seconds, milliseconds, or microseconds. By default it is set to milliseconds.
+```cpp
+void Foo() { ... }
+
+void Bar() 
+{
+    {
+        Profiler p{"Foo() timer", Format::Micro};
+        Foo();
+    }
+}
+```
+Output:
+[Foo() timer] elapsed time: 2029064 us
---
+++ profiler.h
@@ -0,0 +1,66 @@
+#include <cstdio>
+#include <chrono>
+
+enum Format {
+    Second,
+    Milli,
+    Micro
+};
+
+// A simple scope-based timer, starts when object is created and stops when deleted.
+class Profiler
+{
+public:
+    Profiler(const char *Name = "unnamed timer", Format format = Format::Milli)
+        : mName{Name}, mFormat{format}, mStart{std::chrono::high_resolution_clock::now()} {}
+
+    ~Profiler() { PrintElapsedTime(); }
+
+    int64_t GetElapsedTime()
+    {
+        mEnd = std::chrono::high_resolution_clock::now();
+        
+        switch (mFormat)
+        {
+        case Format::Second:
+            return std::chrono::duration_cast<std::chrono::seconds>(mEnd - mStart).count();
+            break;
+
+        case Format::Milli:
+            return std::chrono::duration_cast<std::chrono::milliseconds>(mEnd - mStart).count();
+            break;
+
+        case Format::Micro:
+            return std::chrono::duration_cast<std::chrono::microseconds>(mEnd - mStart).count();
+            break;
+
+        default:
+            return -1;
+            break;
+        }
+    }
+
+    void PrintElapsedTime()
+    {
+        switch (mFormat)
+        {
+        case Format::Second:
+            printf("[%s] elapsed time: %li s\n", mName, GetElapsedTime());
+            break;
+
+        case Format::Milli:
+            printf("[%s] elapsed time: %li ms\n", mName, GetElapsedTime());
+            break;
+
+        case Format::Micro:
+            printf("[%s] elapsed time: %li us\n", mName, GetElapsedTime());
+            break;
+        }
+    }
+
+private:
+    const char *mName;
+    Format mFormat;
+    std::chrono::high_resolution_clock::time_point mStart;
+    std::chrono::high_resolution_clock::time_point mEnd;
+};
